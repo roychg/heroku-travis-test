da33ed47b17c6ed3c8ae0154b8e4587ea959adb4
rc <48433460+roychg@users.noreply.github.com>
2019-04-28 02:42:00 -0700

    updated test script

--- test/index.spec.js
+++ test/index.spec.js
@@ -2,8 +2,7 @@ const expect = require("chai").expect;
 const server = require("../index");
 
 describe("/GET ROOT", () => {
-  it("should return a string", (done) => {
-    
-    expect("ci with travis").to.equal("ci with travis");
+  it("should return a string", async () => {
+     await expect("ci with travis").to.equal("ci with travis");
   });
 });
