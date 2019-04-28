230aa8669244daef1493d8ea8dfc2ece8cff47a4
rc <48433460+roychg@users.noreply.github.com>
2019-04-28 04:49:02 -0700

    updated test script#1

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require("express");
 
 const app = express();
+const bp = require('body-parser')
+
+app.use(bp.json())
 
 app.get("/", (req, res) => {
-  res.send("ci with travis");
+  const testing = [1,2,3,4,5,6]
+  return res.status(200).json({ testing })
 });
 
 const server = app.listen(3000, () => {
--- test/index.spec.js
+++ test/index.spec.js
@@ -1,8 +1,24 @@
 const expect = require("chai").expect;
 const server = require("../index");
+let chaiHttp = require("chai-http");
+let chai = require("chai");
+let should = chai.should()
+
+
+chai.use(chaiHttp);
 
 describe("/GET ROOT", () => {
-  it("should return a string", async () => {
-     await expect("ci with travis").to.equal("ci with travis");
+  it("should return an object", done  => {
+     chai.request(server)
+      .get('/')
+      .end((err, res) => {
+        console.log(res.body)
+        res.should.have.status(200)
+        // expect(`${res.body}`).to.equal()
+        res.body.should.have.property('testing')
+        res.body.testing.should.eql([1,2,3,4,5,6])
+        done()
+      })
+
   });
 });
